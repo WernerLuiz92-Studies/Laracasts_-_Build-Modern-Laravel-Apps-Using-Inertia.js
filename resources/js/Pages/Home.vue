6557893e699c2318c2298a2bc808b0120a5b096e
Werner Luiz Gottschalt <werner@lumiun.com>
2021-12-14 16:20:27 -0300

    Lesson 3: Pages - First Example

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <h1>Hello, {{ name }}!</h1>
+    <p><span v-html="message"></span></p>
+
+    <div>
+        <h2>Tecnologias utilizadas nessa serie:</h2>
+        <ul>
+            <li v-for="framework in frameworks" :key="framework">
+                {{ framework }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props: {
+            name: {
+                required: true,
+                type: String
+            },
+            frameworks: {
+                required: true,
+                type: Array
+            }
+        },
+
+        computed: {
+            message() {
+                let messageFragment = 'Welcome to your new ';
+
+                this.frameworks.forEach(framework => {
+                    if (framework === this.frameworks[this.frameworks.length - 1]) {
+                        messageFragment += '<b>' + framework + '</b> application!';
+                    } else {
+                        messageFragment += '<b>' + framework + '</b> + ';
+                    }
+                });
+
+                return messageFragment
+            }
+        }
+
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return Inertia::render('Home');
+})->name('home');
+
+Route::get('/users', function () {
+    return Inertia::render('Users');
+})->name('users');
+
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
+})->name('settings');
