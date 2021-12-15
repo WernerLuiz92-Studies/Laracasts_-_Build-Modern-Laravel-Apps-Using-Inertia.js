d090580b80046ea00662c829b34b4f290dbc454e
Werner Luiz Gottschalt <werner@lumiun.com>
2021-12-15 17:50:40 -0300

    feat(Pages): Paginas básicas com layout padrão

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -1,10 +1,30 @@
 <template>
-    <h1>Settings</h1>
-
+    <!-- Nav -->
     <Nav />
+    <!-- /End Nav -->
+
+    <header>
+        <!-- Page Banner -->
+        <Banner>
+            Settings
+        </Banner>
+        <!-- /End Page Banner -->
+    </header>
+
+    <main>
+        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <!-- Replace with your content -->
+            <div class="px-4 py-4 sm:px-0">
+                <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+            </div>
+            <!-- /End replace -->
+        </div>
+    </main>
+
 </template>
 
 <script>
+    import Banner from '../Shared/Banner';
     import Nav from '../Shared/Nav';
 
     export default {
@@ -12,6 +32,7 @@
         name: 'Settings',
 
         components: {
+            Banner,
             Nav,
         },
 
