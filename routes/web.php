3643b4e2f3a00565a3e3ad108d806ee65a81b970
Werner Luiz Gottschalt <werner@lumiun.com>
2021-12-16 18:41:37 -0300

    feat(Routes): Created new routes

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,86 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-})->name('home');
+Route::group(['middleware' => ['auth']], function () {
 
-Route::get('/users', function () {
-    sleep(2);
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 
-    return Inertia::render('Users');
-})->name('users');
+    Route::get('/localgroups', function () {
+        return Inertia::render('LocalGroups');
+    })->name('localgroups');
 
-Route::get('/settings', function () {
-    return Inertia::render('Settings');
-})->name('settings');
+    Route::get('/access_control', function () {
+        return Inertia::render('AccessControl');
+    })->name('access_control');
 
-Route::post('/logout', function () {
-    dd('logging the user out');
-})->name('logout');
+    Route::get('/firewall', function () {
+        return Inertia::render('Firewall');
+    })->name('firewall');
+
+    Route::get('/vpns', function () {
+        return Inertia::render('Vpns');
+    })->name('vpns');
+
+    Route::get('/reports', function () {
+        return Inertia::render('Reports');
+    })->name('reports');
+
+    Route::get('/traffic_control', function () {
+        return Inertia::render('TrafficControl');
+    })->name('traffic_control');
+
+    Route::prefix('/networks')->group(function () {
+
+        Route::get('/', function () {
+            return Inertia::render('Networks');
+        })->name('networks');
+
+        Route::get('/custom_dns_record', function () {
+            return Inertia::render('CustomDnsRecord');
+        })->name('custom_dns_record');
+
+        Route::get('/conditional_forwarding_dns', function () {
+            return Inertia::render('ConditionalForwardingDns');
+        })->name('conditional_forwarding_dns');
+
+    });
+
+    Route::prefix('/clientips')->group(function () {
+
+        Route::get('/clientips', function () {
+            return Inertia::render('ClientIps');
+        })->name('clientips');
+
+        Route::get('/ignored_macs', function () {
+            return Inertia::render('IgnoredMacs');
+        })->name('ignored_macs');
+
+    });
+
+    Route::prefix('/users')->group(function () {
+
+        Route::get('/', function () {
+            return Inertia::render('Users', [
+                'time' => now()->toTimeString(),
+            ]);
+        })->name('users');
+
+        Route::get('/settings', function () {
+            return Inertia::render('Settings');
+        })->name('settings');
+        
+        Route::post('/logout', function () {
+            dd('logging the user out');
+        })->name('logout');
+
+    });
+
+});
+
+Route::fallback(function () {
+    return Inertia::render('Dashboard');
+});
+
+
