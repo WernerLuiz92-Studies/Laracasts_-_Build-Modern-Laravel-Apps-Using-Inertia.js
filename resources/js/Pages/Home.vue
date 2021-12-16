a164e0899f21580f70869b436f8729d09d3e787f
Werner Luiz Gottschalt <werner@lumiun.com>
2021-12-16 09:50:28 -0300

    refactor(Vetur): autoformat code quality

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,36 +5,37 @@
 
     <header>
         <!-- Page Banner -->
-        <Banner>
-            Home
-        </Banner>
+        <Banner> Home </Banner>
         <!-- /End Page Banner -->
     </header>
 
     <main>
-        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="container mx-auto py-6 sm:px-6 lg:px-8">
             <!-- Replace with your content -->
             <div class="px-4 py-4 sm:px-0">
-                <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                <div
+                    class="
+                        border-4 border-dashed border-gray-200
+                        rounded-lg
+                        h-96
+                    "
+                />
             </div>
             <!-- /End replace -->
         </div>
     </main>
-
 </template>
 
 <script>
-    import Banner from '../Shared/Banner';
-    import Nav from '../Shared/Nav';
+import Banner from "../Shared/Banner";
+import Nav from "../Shared/Nav";
 
-    export default {
+export default {
+    name: "Home",
 
-        name: 'Home',
-
-        components: {
-            Banner,
-            Nav,
-        },
-
-    }
+    components: {
+        Banner,
+        Nav,
+    },
+};
 </script>
--- resources/js/Shared/Banner.vue
+++ resources/js/Shared/Banner.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="bg-white shadow">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h1 class="text-3xl font-bold leading-tight text-gray-900">
                 <slot></slot>
             </h1>
@@ -9,9 +9,7 @@
 </template>
 
 <script>
-    export default {
-
-        name: 'Banner',
-
-    }
+export default {
+    name: "Banner",
+};
 </script>
