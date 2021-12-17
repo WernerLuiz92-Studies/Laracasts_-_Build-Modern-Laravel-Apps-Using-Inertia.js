747d5d6be502617744a44794fbd1514477d522b5
Werner Luiz Gottschalt <werner@lumiun.com>
2021-12-17 16:51:42 -0300

    style(TailwindCSS Colors): Custom colors and aliases

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./resources/**/*.blade.php",
@@ -5,7 +7,29 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: {
+          100: '#FEF3C7',
+          400: '#FBBF24',
+          800: '#92400E',
+        },
+        green: colors.emerald,
+        'light-blue': colors.sky,
+        blue: {
+          50: '#F3FAFE',
+          100: "#E8F5FD",
+          200: "#C4E7F9",
+          300: "#5CBDF1",
+          400: "#5BBAEE",
+          500: "#149DE7",
+          600: "#128DD0",
+          700: "#0F76AD",
+          800: "#0C5E8B",
+          900: "#0A4D71",
+        },
+      },
+    },
   },
   plugins: [],
 }
