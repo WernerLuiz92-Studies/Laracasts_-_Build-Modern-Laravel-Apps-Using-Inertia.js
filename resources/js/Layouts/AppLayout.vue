a068de596e4f0b3c8fc8fbd2d87216343ea81cd8
Werner Luiz Gottschalt <werner@lumiun.com>
2021-12-21 18:08:36 -0300

    feat(Layout): Created a Layout to the pages

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex flex-col h-screen">
+        <header>
+            <Nav />
+        </header>
+
+        <main class="mb-auto">
+            <slot />
+        </main>
+
+        <footer>
+            <Footer />
+        </footer>
+    </div>
+</template>
+
+<script>
+import Nav from "../Shared/Nav";
+import Footer from "../Shared/Footer";
+
+export default {
+    components: {
+        Nav,
+        Footer,
+    },
+};
+</script>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,69 +1,70 @@
 <template>
-    <!-- Nav -->
-    <Nav />
-    <!-- /End Nav -->
+    <AppLayout>
+        <header>
+            <!-- Page Banner -->
+            <Banner> Usuários </Banner>
+            <!-- /End Page Banner -->
+        </header>
 
-    <header>
-        <!-- Page Banner -->
-        <Banner> Usuários </Banner>
-        <!-- /End Page Banner -->
-    </header>
-
-    <main>
-        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <!-- Replace with your content -->
-            <div class="px-4 py-4 sm:px-0">
-                <div
-                    class="
-                        border-4 border-dashed border-gray-200
-                        rounded-lg
-                        h-[1000px]
-                    "
-                >
+        <main>
+            <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <!-- Replace with your content -->
+                <div class="px-4 py-4 sm:px-0">
                     <div
                         class="
-                            w-full
-                            h-full
-                            flex flex-col
-                            items-center
-                            justify-center
+                            border-4 border-dashed border-gray-200
+                            rounded-lg
+                            h-[1000px]
                         "
                     >
-                        <p>The current time is {{ time }}.</p>
-                        <Link
+                        <div
                             class="
-                                inline-flex
+                                w-full
+                                h-full
+                                flex flex-col
                                 items-center
-                                mt-2
-                                p-2
-                                border border-transparent
-                                rounded-full
-                                shadow-sm
-                                text-white
-                                bg-blue-600
-                                hover:bg-light-blue-700
-                                focus:outline-none
-                                focus:ring-2
-                                focus:ring-offset-2
-                                focus:ring-light-blue-500
+                                justify-center
                             "
-                            href="/users"
-                            as="button"
-                            preserve-scroll
                         >
-                            <RefreshIcon class="h-5 w-5" aria-hidden="true" />
-                        </Link>
+                            <p>The current time is {{ time }}.</p>
+                            <Link
+                                class="
+                                    inline-flex
+                                    items-center
+                                    mt-2
+                                    p-2
+                                    border border-transparent
+                                    rounded-full
+                                    shadow-sm
+                                    text-white
+                                    bg-blue-600
+                                    hover:bg-light-blue-700
+                                    focus:outline-none
+                                    focus:ring-2
+                                    focus:ring-offset-2
+                                    focus:ring-light-blue-500
+                                "
+                                href="/users"
+                                as="button"
+                                preserve-scroll
+                            >
+                                <RefreshIcon
+                                    class="h-5 w-5"
+                                    aria-hidden="true"
+                                />
+                            </Link>
+                        </div>
                     </div>
                 </div>
+                <!-- /End replace -->
             </div>
-            <!-- /End replace -->
-        </div>
-    </main>
+        </main>
+    </AppLayout>
 </template>
 
 <script>
+import AppLayout from "../Layouts/AppLayout";
 import Banner from "../Shared/Banner";
-import Nav from "../Shared/Nav";
 import { Link } from "@inertiajs/inertia-vue3";
 import { RefreshIcon } from "@heroicons/vue/outline";
 
@@ -71,8 +72,8 @@ export default {
     name: "Users",
 
     components: {
+        AppLayout,
         Banner,
-        Nav,
         Link,
         RefreshIcon,
     },
