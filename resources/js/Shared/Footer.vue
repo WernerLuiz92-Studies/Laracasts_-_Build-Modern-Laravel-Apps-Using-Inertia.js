3d33ad3a20ae38456de254e6295354430af85595
Werner Luiz Gottschalt <werner@lumiun.com>
2021-12-21 18:08:10 -0300

    feat(Footer): Created a footer to implement de layout of the pages

---
+++ resources/js/Shared/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="min-h-full static bottom-0">
+        <div
+            class="
+                max-w-full
+                border-t border-gray-200
+                mx-auto
+                px-4
+                sm:px-6
+                lg:px-8
+            "
+        >
+            <div
+                class="
+                    py-8
+                    text-sm text-gray-500 text-center
+                    sm:text-left
+                    flex flex-col
+                    items-center
+                    space-y-2
+                "
+            >
+                <span class="block sm:inline text-gray-600"
+                    >&copy; {{ year }} Lumiun Internet Security. Todos os
+                    direitos reservados.</span
+                >
+                <span class="block sm:inline text-xs text-gray-600"
+                    >Fuso horário America/Sao_Paulo
+                    <Link class="text-blue-500 ml-2" href="#"
+                        >Alterar</Link
+                    ></span
+                >
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+export default {
+    name: "Footer",
+
+    components: {
+        Link,
+    },
+
+    data() {
+        let now = new Date();
+
+        return {
+            year: now.getFullYear(),
+        };
+    },
+};
+</script>
