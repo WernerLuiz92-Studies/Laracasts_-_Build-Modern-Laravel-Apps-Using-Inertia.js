c906cf77fc1ea900650f2a71e4e57d708e9c8827
Werner Luiz Gottschalt <werner@lumiun.com>
2021-12-22 14:28:22 -0300

    build(Axios): install of Axios package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,15 @@
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(VueAxios, axios)
       .mount(el);
   },
 });
